1bf0160eea524681fe8614a4d6564a859484ec32
Alok Singh <alok.singh@deptagency.com>
2026-07-12 13:06:27 +0530

    js file update

--- blocks/bold-promises/bold-promises.js
+++ blocks/bold-promises/bold-promises.js
@@ -1,9 +1,11 @@
 import dataMapWpObj from '../../scripts/constant.js';
 
 export default function decorate(block) {
+    debugger;
   dataMapWpObj.CLASS_PREFIXES = [
     'bp-item',
     'bp-subitem',
+    'bp-subitem-finelsub',
   ];
   dataMapWpObj.addIndexed(block);
 }
